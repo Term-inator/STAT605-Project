be7489c4ae80839d24126bece1e464833641fdb2
Term-inator <1121745627@qq.com>
2024-02-16 08:43:08 -0600

    remove comment

--- split.py
+++ split.py
@@ -5,7 +5,7 @@ def copy_json_lines_to_compressed_files(input_file, output_dir, lines_per_file):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
-    buffer = []  # 初始化缓冲区
+    buffer = []
     file_count = 0
     line_count = 0
     
@@ -21,10 +21,9 @@ def copy_json_lines_to_compressed_files(input_file, output_dir, lines_per_file):
                 print(output_file)
                 with gzip.open(output_file, 'wt', encoding='utf-8') as out_f:
                     out_f.writelines(buffer)
-                buffer = []  # 清空缓冲区
+                buffer = []
                 print("ok")
 
-        # 处理剩余的行
         if buffer:
             file_count += 1
             output_file = os.path.join(output_dir, f'json_{file_count}.json.gz')
@@ -34,11 +33,9 @@ def copy_json_lines_to_compressed_files(input_file, output_dir, lines_per_file):
     print("done.")
 
 
-# 输入和输出路径
 name = 'Video_Games'
 input_file = f'data/{name}.json.gz'
 output_dir = f'data/{name}/'
 lines_per_file = 400000
 
-# 调用函数
 copy_json_lines_to_compressed_files(input_file, output_dir, lines_per_file)
